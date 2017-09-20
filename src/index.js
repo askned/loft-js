72c6cafb8a74de9416b5b2e8d8b3008e0166a912
askden <askden8@gmail.com>
2017-09-20 10:32:32 +0300

    tt

--- src/index.js
+++ src/index.js
@@ -38,7 +38,14 @@ function loadAndSortTowns() {
                 // for(var i =0; i < myArr.length;i++){
                 //     array[i]=myArr[i].name;
                 // }
-                myArr.name.sort();
+                myArr.sort(compareCity);
+                
+                function compareCity(objCity1, objCity2) {
+                        if (objCity1.name < objCity2.name) {
+                            return -1
+                        } else if (objCity1.name > objCity2.name) {
+                            return 1
+                 }
                 console.log(myArr);
                 return myArr
                 }
